06f98c8fd82c2e269742480275fe410164bf6ac5
hamza-alkharouf <hamzaalkharouf5@gmail.com>
2022-05-31 00:34:35 +0300

    update

--- resources/views/complaints/details/index.blade.php
+++ resources/views/complaints/details/index.blade.php
@@ -39,6 +39,7 @@
                     <div class="card ">
                         <div class="card-header">
                             <h3 class="card-title">{{ __('All Complaint') }}</h3>
+
                             <div class="card-tools">
                                 <!-- Collapse Button -->
                                 <select class="form-control" name="forma" onchange="location = this.value;">
@@ -54,6 +55,8 @@
                                 </select>
                             </div>
                         </div>
+                        <x-auth-validation-errors class="m-4" :errors="$errors" />
+
                         <!-- /.card-header -->
                         <div class="card-body table-responsive">
                             <table id="example" class="table table-striped table-bordered" style="width:100%">
@@ -101,26 +104,23 @@
                                                     {{ $complaintsForm['hour'] }}</span></td>
                                             <td>
                                                 <div class="btn-group">
-                                                    <a href="{{ route('complaints.details.complaintDecline', $complaintsForm['id']) }}"
-                                                        type="button" class="btn btn-danger m-1"
-                                                        style="font-size:13px">Decline</a>
-                                                    <a href="{{ route('complaints.details.complaintResolved', [$complaintsForm['id'],$complaintsForm['type']]) }}"
-                                                        type="button" class="btn btn-success m-1"
-                                                        style="font-size:13px">Resolved</a>
-
+                                                    <a href="{{ route('complaints.details.complaintResolvedDefult', $complaintsForm['id']) }}"
+                                                    type="button" class="btn btn-success m-1"
+                                                    style="font-size:13px">{{ __('Resolved') }}</a>
+                                                    <a 
+                                                    type="button" class="btn btn-danger m-1"  data-toggle="modal" data-target="#modal-default-{{ $complaintsForm['id'] }}"style="font-size:13px">{{ __('Decline') }}</a>
                                                     @if (Auth::user()->type != 'deanDepartment')
-                                                        <a href="{{ route('complaints.details.complaintDeanDepartment', $complaintsForm['id']) }}"
-                                                            type="button" class="btn btn-primary m-1"
-                                                            style="font-size:13px">Dean department</a>
-                                                    @endif
-
+                                                    <a href="{{ route('complaints.details.complaintDeanDepartmentDefult', $complaintsForm['id']) }}"
+                                                        type="button" class="btn btn-primary m-1"
+                                                        style="font-size:13px">{{ __('Dean department') }}</a>
+                                                    @endif 
                                                 </div>
                                             </td>
                                         </tr>
                                     @empty
                                         <tr>
                                             <td colspan="10">
-                                                No Complaints Found.
+                                                {{ __('No Complaints Found.') }}
                                             </td>
                                         </tr>
                                     @endforelse
@@ -139,51 +139,55 @@
         <!-- /.container-fluid -->
     </section>
     <!-- /.content -->
+    
+    @forelse ($complaintsForms as $complaintsForm)
+    <div class="modal fade" id="modal-default-{{ $complaintsForm['id'] }}">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h4 class="modal-title">{{ __('Decline Order') }}</h4>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <p>{{ __('Name Student') }} : {{ $complaintsForm->user->name }}</p>
+              <p>{{ __('ID') }} : {{ $complaintsForm->user->type_username_id }}</p>
+              <p>{{ __('Type') }} : {{ $complaintsForm['type'] }}</p>
+              <p>{{ __('Course Number') }} : {{ $complaintsForm['course_number'] }}</p>
+              <p>{{ __('Section') }} : {{ $complaintsForm['section'] }}</p>
+              <p>{{ __('Course Name') }} : {{ $complaintsForm['course_name'] }}</p>
+              <p>{{ __('Teacher Name') }} : {{ $complaintsForm['teacher_name'] }}</p>
+              <p>{{ __('Days') }} : {{ $complaintsForm['days'] }}</p>
+              <p>{{ __('Hour') }} : {{ $complaintsForm['hour'] }}</p>
 
-</div>
+              <form action="{{ route('complaints.details.complaintDeclineDefult', $complaintsForm['id']) }}" method="post">
+                @csrf
+    
+                <div class="form-group">
+                    <label>{{ __('Notes') }}</label>
+                    <textarea class="form-control" rows="3" name="notes" placeholder="{{ __('Enter Your Notes') }}"></textarea>
+                  </div>
+                  <div class="modal-footer justify-content-between">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">{{ __('Close') }}</button>
+                    <button  type="submit"  class="btn btn-danger">{{ __('Decline') }}</button>
 
-{{-- 
-<button type="button" class="btn btn-danger" data-toggle="modal" data-target="#modal-default">
-    Decline
-  </button>
-
-<div class="modal fade" id="modal-default">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h4 class="modal-title">Decline Order</h4>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
+                  </div>
+                </form>
+            </div>
+    
+          </div>
+          <!-- /.modal-content -->
         </div>
-        <div class="modal-body">
-          <p>{{ __('Name Student') }} : {{ $complaintsForm->user->name }}</p>
-          <p>{{ __('ID') }} : {{ $complaintsForm->user->type_username_id }}</p>
-          <p>{{ __('Type') }} : {{ $complaintsForm['type'] }}</p>
-          <p>{{ __('Course Number') }} : {{ $complaintsForm['course_number'] }}</p>
-          <p>{{ __('Section') }} : {{ $complaintsForm['section'] }}</p>
-          <p>{{ __('Course Name') }} : {{ $complaintsForm['course_name'] }}</p>
-          <p>{{ __('Teacher Name') }} : {{ $complaintsForm['teacher_name'] }}</p>
-          <p>{{ __('Days') }} : {{ $complaintsForm['days'] }}</p>
-          <p>{{ __('Hour') }} : {{ $complaintsForm['hour'] }}</p>
-          <form action="{{ route('complaints.details.complaintDecline',$complaintsForm['id']) }}" method="post">
-            @csrf
-
-            <div class="form-group">
-                <label>{{ __('Notes') }}</label>
-                <textarea class="form-control" rows="3" name="notes" placeholder="{{ __('Enter Your Notes') }}"></textarea>
-              </div>
-              <div class="modal-footer justify-content-between">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <a href="{{ route('complaints.details.complaintDecline',$complaintsForm['id']) }}" type="button" class="btn btn-danger m-1"
-                style="font-size:13px">Decline</a>
-              </div>
-            </form>
-        </div>
-
-      </div>
-      <!-- /.modal-content -->
+        <!-- /.modal-dialog -->
     </div>
-    <!-- /.modal-dialog -->
+      <!-- /.modal --> 
+      @empty
+      <tr>
+          <td colspan="10">
+              No Complaints Found.
+          </td>
+      </tr>
+  @endforelse
 </div>
-  <!-- /.modal --> --}}
+
