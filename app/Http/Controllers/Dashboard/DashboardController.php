72d63489e20fa7364944c038aa034bf7354d7684
mohmmaddwekat <57283690+mohmmaddwekat@users.noreply.github.com>
2022-05-30 13:12:28 +0300

    update

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Dashboard\Controller;
 use App\Models\Admin\Course;
+use App\Models\Complaint\ComplaintsForm;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -12,12 +13,33 @@ class DashboardController extends Controller
     public function index()
     {
         $user = Auth::user();
-        // if ($user->type == 'student') {
-        //     $this->dashboardTemplate('student', __('Student Dashboard'), ['student' => $user]);
-        // } else {
-            $this->dashboardTemplate('index', __('Dashboard'), ['user' => $user]);
-        // }
+        $statisticOne = 0;
+        $statisticTwo = 0;
+        $statisticThree = 0;
+        $statisticFour = 0;
+        if ($user->type == 'student') {
+            $statisticOne = $user->coursesStudent()->where('status', '=', 'Fail')->get()->count();
+            $statisticTwo = $user->coursesStudent()->where('status', '=', 'Success')->get()->count();
+            $statisticThree = $user->coursesStudent()->where('status', '!=', 'Fail')->where('status', '!=', 'Success')->get()->count();
+            $statisticFour = $user->complaint->count();
+            $complaintsForm = ComplaintsForm::where('user_id', Auth::id())->get();
+            $this->dashboardTemplate('index', __('Dashboard'), [
+                'user' => $user,
+                'statisticOne' => $statisticOne,
+                'statisticTwo' => $statisticTwo,
+                'statisticThree' => $statisticThree,
+                'statisticFour' => $statisticFour,
+                'complaintsForms' =>$complaintsForm,
+            ]);
+        } else {
+        }
+        
     }
+
+
+
+
+
     public function yearCourse($year)
     {
         $track = Course::where([['year', '=', $year], ['track', '!=', null]])->get(['track']);
--- resources/views/dashboard/student.blade.php
+++ resources/views/dashboard/student.blade.php
@@ -21,36 +21,141 @@
     <section class="content">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-12">
-                    <x-slot name="header">
-                        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                            {{ __('Dashboard') }}
-                        </h2>
-                    </x-slot>
-                    <div class="py-12">
-                        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                            <div class="bg-white  shadow-sm sm:rounded-lg">
-                                <div class="p-6 bg-white border-b border-gray-200">
-                                    <div class="row">
-                                        <div class="col-md-12">
-
-
-                                            <div class="m-5 p-5 text-center">
-                                                
-
-                                            </div>
-
-                                            <!-- /.card -->
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                <div class="col-12 col-sm-6 col-md-3">
+                  <div class="info-box">
+                    <span class="info-box-icon bg-info elevation-1"><i class="fas fa-cog"></i></span>
+      
+                    <div class="info-box-content">
+                      <span class="info-box-text">Failing courses</span>
+                      <span class="info-box-number">
+                        {{$statisticOne}}
+                      </span>
                     </div>
-                    <!-- /.card -->
+                    <!-- /.info-box-content -->
+                  </div>
+                  <!-- /.info-box -->
                 </div>
                 <!-- /.col -->
-            </div>
+                <div class="col-12 col-sm-6 col-md-3">
+                  <div class="info-box mb-3">
+                    <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-thumbs-up"></i></span>
+      
+                    <div class="info-box-content">
+                      <span class="info-box-text">Successful courses</span>
+                      <span class="info-box-number">{{$statisticTwo}}</span>
+                    </div>
+                    <!-- /.info-box-content -->
+                  </div>
+                  <!-- /.info-box -->
+                </div>
+                <!-- /.col -->
+      
+                <!-- fix for small devices only -->
+                <div class="clearfix hidden-md-up"></div>
+      
+                <div class="col-12 col-sm-6 col-md-3">
+                  <div class="info-box mb-3">
+                    <span class="info-box-icon bg-success elevation-1"><i class="fas fa-shopping-cart"></i></span>
+      
+                    <div class="info-box-content">
+                      <span class="info-box-text">Courses you haven't studied</span>
+                      <span class="info-box-number">{{$statisticThree}}</span>
+                    </div>
+                    <!-- /.info-box-content -->
+                  </div>
+                  <!-- /.info-box -->
+                </div>
+                <!-- /.col -->
+                <div class="col-12 col-sm-6 col-md-3">
+                  <div class="info-box mb-3">
+                    <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-users"></i></span>
+      
+                    <div class="info-box-content">
+                      <span class="info-box-text">Complaint</span>
+                      <span class="info-box-number">{{$statisticFour}}</span>
+                    </div>
+                    <!-- /.info-box-content -->
+                  </div>
+                  <!-- /.info-box -->
+                </div>
+                <!-- /.col -->
+              </div>
+              <div class="row">
+                <div class="col-12">
+                  <div class="card " >
+                    <div class="card-header">
+                      <h3 class="card-title">{{__('All Complaint')}}</h3>
+                      <div class="card-tools">
+                          <!-- Collapse Button -->
+                          @can('add form complaints')
+                          <a href="{{route('complaints.form.create')}}" class="btn btn-block btn-outline-primary">{{__('Create Complaint')}}</a>
+                          @endcan
+                      </div>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body table-responsive">
+                       <table id="example" class="table table-striped table-bordered" style="width:100%">
+                          <thead>
+                            
+                              <tr>
+        
+                                  <th>#</th>
+                                  <th>{{ __('Name Student') }}</th>
+                                  <th>{{ __('ID Student') }}</th>
+                                  <th>{{__('Type')}}</th>
+                                  <th>{{__('Course Number')}}</th>
+                                  <th>{{__('Section')}}</th>
+                                  <th>{{__('Course Name')}}</th>
+                                  <th>{{__('Teacher Name')}}</th>
+                                  <th>{{__('Days')}}</th>
+                                  <th>{{__('Hour')}}</th>
+                                  <th>{{__('Status')}}</th>
+        
+                              </tr>
+                          </thead>
+                          <tbody>
+                            @forelse ($complaintsForms as $complaintsForm)
+                            <tr>
+                            <td>
+                              <a class="font-weight-bold">
+                                  {{  $complaintsForm['id'] }}
+                              </a>
+                          </td>
+                          <td>
+                            <span class="font-weight-normal">
+                                {{  $complaintsForm->user->name  }}</span>
+                        </td>
+                        <td><span class="font-weight-normal">
+                                {{ $complaintsForm->user->type_username_id }}</span></td>
+                            <td><span class="font-weight-normal"> {{ $complaintsForm['type'] }}</span></td>
+                            <td><span class="font-weight-normal"> {{ $complaintsForm['course_number'] }}</span></td>
+                            <td><span class="font-weight-normal"> {{ $complaintsForm['section'] }}</span></td>
+                            <td><span class="font-weight-normal"> {{ $complaintsForm['course_name'] }}</span></td>
+                            <td><span class="font-weight-normal"> {{ $complaintsForm['teacher_name'] }}</span></td>
+                            <td><span class="font-weight-normal"> {{ $complaintsForm['days'] }}</span></td>
+                            <td><span class="font-weight-normal"> {{ $complaintsForm['hour'] }}</span></td>
+                            <td class="@if ($complaintsForm['status'] == 'Declined') bg-danger bg-gradient w-10 h-5 @elseif ($complaintsForm['status'] == 'Resolved') bg-success bg-gradient  w-10 h-5 @elseif ($complaintsForm['status'] == 'In progress By the Dean of the department') bg-info bg-gradient  w-10 h-5  @else  bg-secondary bg-gradient  w-10 h-5  @endif "><span class="font-weight-normal"> 
+                            
+                            {{ $complaintsForm['status'] }}
+                            </span></td>
+                          </tr>
+                            @empty
+                            <tr>
+                                <td colspan="10">
+                                    No Complaints Found.
+                                </td>
+                            </tr>
+                        @endforelse
+                          </tbody>
+        
+                      </table>
+                    </div>
+                    <!-- /.card-body -->
+                  </div>
+                  <!-- /.card -->
+                </div>
+                <!-- /.col -->
+              </div>
             <!-- /.row -->
         </div>
         <!-- /.container-fluid -->
