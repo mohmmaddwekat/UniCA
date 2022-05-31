061b5c81e3fc10002ad40ee7cfaabf2c27bdd5aa
MalakNidal <malak.awawdeh@stu.najah.edu>
2022-05-31 19:01:54 +0300

    Language dropdown

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link href ="/assets/css/welcome.css" rel = "stylesheet">
 <link href ="/assets/css/app.css" rel ="stylesheet">
+</head>
+    
 
 <body class="bodyLogin">
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
+            <div class="container">
+            <img src="{{ url('/assets/img/background3.jpeg') }}" alt="AdminLTE Logo"
+              class="brand-image img-circle elevation-3" style="opacity: .8">
+                <a class="navbar-brand" href="#page-top">UniCA</a>
+                <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        @if (Route::has('login'))
+                    @auth
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="{{ url('/login') }}">{{ __('Dashboard') }}</a></li>
+
+                    @else
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="{{ url('/login') }}">{{ __('Log In') }}</a></li>
+
+                        @if (Route::has('student.register'))
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="{{ url('/') }}">{{ __('Home Page') }}</a></li>
+                        @endif
+                    @endauth
+                        @endif
+                    </ul>
+                </div>
+            </div>
+    </nav>
+<!-- Masthead-->
+<br><br><br><br><br>
 <div class="loginContainer">
 <x-guest-layout>
     <x-auth-card>
@@ -46,15 +82,12 @@
                     <span class="ml-2">{{ __('Remember me') }}</span>
                 </label>
             </div>
-            <br>
-
             <div class="flex">
                 @if (Route::has('student.register'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('student.register') }}">
                         {{ __('Register') }}
                     </a>
                 @endif
-
                 <x-button class="ml-3">
                     {{ __('Log in') }}
                 </x-button>
@@ -64,3 +97,4 @@
 </x-guest-layout>
 </div>
 </body>
+</html>
